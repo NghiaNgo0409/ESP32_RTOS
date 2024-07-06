ef1c8f04582ded90c92a40c3196bd554ab89e118
MSI\KAI <nhanksd85@gmail.com>
2024-07-06 12:50:13 +0700

    Task 2: Temperature and Humidity (DHT20) and LCD display

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,22 @@
 #include <Adafruit_NeoPixel.h>
+#include <DHT20.h>
+#include <LiquidCrystal_I2C.h>;
+
 // Define your tasks here
 void TaskBlink(void *pvParameters);
 void TaskTemperatureHumidity(void *pvParameters);
 
 //Define your components here
 Adafruit_NeoPixel pixels3(4, D3, NEO_GRB + NEO_KHZ800);
-
+DHT20 dht20;
+LiquidCrystal_I2C lcd(33,16,2);
 
 void setup() {
 
   // Initialize serial communication at 115200 bits per second:
   Serial.begin(115200); 
+  dht20.begin();
+  lcd.begin();
   
   xTaskCreate( TaskBlink, "Task Blink" ,2048  ,NULL  ,2 , NULL);
   xTaskCreate( TaskTemperatureHumidity, "Task Temperature" ,2048  ,NULL  ,2 , NULL);
@@ -38,9 +44,9 @@ void TaskBlink(void *pvParameters) {  // This is a task.
 
   while(1) {                          
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED ON
-    delay(2000);
+    delay(200);
     digitalWrite(LED_BUILTIN, LOW);  // turn the LED OFF
-    delay(2000);
+    delay(200);
   }
 }
 
@@ -50,6 +56,16 @@ void TaskTemperatureHumidity(void *pvParameters) {  // This is a task.
 
   while(1) {                          
     Serial.println("Task Temperature and Humidity");
+    dht20.read();
+    Serial.println(dht20.getTemperature());
+    Serial.println(dht20.getHumidity());
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(dht20.getTemperature());
+    lcd.setCursor(0, 1);
+    lcd.print(dht20.getHumidity());
+
     delay(5000);
   }
 }
