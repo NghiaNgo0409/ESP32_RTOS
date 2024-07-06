12cfab34cdc76b85370f2066a64dbbd5415fff40
MSI\KAI <nhanksd85@gmail.com>
2024-07-06 13:17:11 +0700

    Task 4: Light sensor and RGB

--- main/main.ino
+++ main/main.ino
@@ -6,9 +6,10 @@
 void TaskBlink(void *pvParameters);
 void TaskTemperatureHumidity(void *pvParameters);
 void TaskSoilMoistureAndRelay(void *pvParameters);
+void TaskLightAndLED(void *pvParameters);
 
 //Define your components here
-Adafruit_NeoPixel pixels3(4, D3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels3(4, D5, NEO_GRB + NEO_KHZ800);
 DHT20 dht20;
 LiquidCrystal_I2C lcd(33,16,2);
 
@@ -18,10 +19,12 @@ void setup() {
   Serial.begin(115200); 
   dht20.begin();
   lcd.begin();
+  pixels3.begin();
   
   xTaskCreate( TaskBlink, "Task Blink" ,2048  ,NULL  ,2 , NULL);
   xTaskCreate( TaskTemperatureHumidity, "Task Temperature" ,2048  ,NULL  ,2 , NULL);
   xTaskCreate( TaskSoilMoistureAndRelay, "Task Soild Relay" ,2048  ,NULL  ,2 , NULL);
+  xTaskCreate( TaskLightAndLED, "Task Light LED" ,2048  ,NULL  ,2 , NULL);
   
   //Now the task scheduler is automatically started.
   Serial.printf("Basic Multi Threading Arduino Example\n");
@@ -79,7 +82,7 @@ void TaskSoilMoistureAndRelay(void *pvParameters) {  // This is a task.
 
   while(1) {                          
     Serial.println("Task Soild and Relay");
-    Serial.println(analogRead(A0));
+    //Serial.println(analogRead(A0));
     
     if(analogRead(A0) > 500){
       digitalWrite(D3, LOW);
@@ -91,3 +94,27 @@ void TaskSoilMoistureAndRelay(void *pvParameters) {  // This is a task.
   }
 }
 
+
+void TaskLightAndLED(void *pvParameters) {  // This is a task.
+  //uint32_t blink_delay = *((uint32_t *)pvParameters);
+
+  while(1) {                          
+    Serial.println("Task Light and LED");
+    Serial.println(analogRead(A1));
+    if(analogRead(A1) < 350){
+      pixels3.setPixelColor(0, pixels3.Color(255,0,0));
+      pixels3.setPixelColor(1, pixels3.Color(255,0,0));
+      pixels3.setPixelColor(2, pixels3.Color(255,0,0));
+      pixels3.setPixelColor(3, pixels3.Color(255,0,0));
+      pixels3.show();
+    }
+    if(analogRead(A1) > 550){
+      pixels3.setPixelColor(0, pixels3.Color(0,0,0));
+      pixels3.setPixelColor(1, pixels3.Color(0,0,0));
+      pixels3.setPixelColor(2, pixels3.Color(0,0,0));
+      pixels3.setPixelColor(3, pixels3.Color(0,0,0));
+      pixels3.show();
+    }
+    delay(1000);
+  }
+}
